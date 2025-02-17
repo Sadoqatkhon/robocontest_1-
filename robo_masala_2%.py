6953569b4788712db79219d768e3ecb03cd90f15
Sadokatkhon <sadoqatxonabdullayeva@gmail.com>
2025-02-18 01:06:42 +0500

    simple

---
+++ robo_masala_2%.py
@@ -0,0 +1,18 @@
+# Robocontest masalalari 2 %
+# #0004 Direktor tashrifi  1-urinishda accepted:)
+# with open('input.txt','r') as f:
+#     a,b = f.readline().split()
+# a,b = int(a),int(b)
+# r = str(a*b)
+# with open('output.txt','w') as f:
+#     f.write(r)
+
+# 0043 O'rin almashtirish
+# with open('input.txt','r') as f:
+#     n,m=f.readline().split()
+# r=f"{m} {n}"
+# with open('output.txt','w') as f:
+#     f.write(r)
+
+
+
