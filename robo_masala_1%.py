5a73590d6ed9a219122212b92dc7994ec786ca39
Sadokatkhon <sadoqatxonabdullayeva@gmail.com>
2025-02-18 01:20:44 +0500

    Wrong file was uploaded before

---
+++ robo_masala_1%.py
@@ -0,0 +1,82 @@
+# #Robocontest masalalari 1% dan boshlab
+# 0492 Mashenik - har doim o'ziga Tuzni 1-bo'lib tarqatishi kerak
+# with open('input.txt','r') as f:
+#     n=f.readline()
+# with open('output.txt','w') as f:
+#     f.write(1)
+
+# # 0707 Mr.quloq - accepted
+# with open('input.txt','r') as f:
+#     t=f.readline()
+# with open('output.txt','w') as f:
+#     f.write(t)
+
+
+# # 0812 O'zing topda shartini - shartini topolmadim
+# with open('input.txt','r') as f:
+#     n=f.readline()
+ 
+# with open('output.txt','w') as f:
+    # f.write(r)
+
+# # 0938 Asilbek yo'qotgan son - accepted : a+b+1 = 1 >> a=-b
+# with open('input.txt','r') as f:
+#     a=int(f.readline())
+# b=str(-a)
+# with open('output.txt','w') as f:
+#     f.write(b)
+
+# # 1091 Taqqoslash - Tadaaam!  Accepted
+# with open('input.txt','r') as f:
+#     a,b,c=f.readline().split()
+# a,b,c=int(a),int(b),int(c)
+# if a==b and a==c:
+#     n=1
+# elif (a==b and a!=c) or (a!=b and a==c):
+#     n=2
+# else:
+#     n=3
+# with open('output.txt','w') as f:
+#     f.write(str(n))
+
+# 1089 Natural son #2
+# nums = [0,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100]
+# writtens = ["","bir","ikki","uch","to'rt","besh","olti","yetti","sakkiz","to'qqiz","o'n","yigirma","o'ttiz","qirq","ellik","oltmish","yetmish","sakson","to'qson","yuz"]
+# num_written = dict(zip(nums,writtens))
+# # print(num_written)
+# with open('input.txt','r') as f:
+#     n=int(f.readline())
+# if n==1000:
+#     r=f"bir ming"
+
+# with open('output.txt','w') as f:
+    # f.write(r)
+
+# # 1092 Juft karrali
+# with open('input.txt','r') as f:
+#     n=int(f.readline())
+# if n%2==0:
+#     r=n
+# else:
+#     r=2*n
+# with open('output.txt','w') as f:
+#     f.write(str(r))
+
+# # 1109 Damino toshlari -accepted
+# with open('input.txt','r') as f:
+#     n,m=f.readline().split()
+# n,m=int(n),int(m)
+# s=n*m
+# r=s//2
+# with open('output.txt','w') as f:
+#     f.write(str(r))
+
+# # 0043 O'rin almashtirish
+# with open('input.txt','r') as f:
+#     n,m=f.readline().split()
+# r=f"{m} {n}"
+# with open('output.txt','w') as f:
+#     f.write(r)
+
+
+
